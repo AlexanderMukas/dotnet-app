8ea4756a4c1344a83b80086c145bca6efb398440
alexa <alexa@DESKTOP-OPTF0E2>
2020-08-22 23:35:16 +0300

    Second Test Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,16 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static double SumTwoNum(double one, double two)
+        {
+            return one + two;
+        }
         static void Main(string[] args)
         {
+            double x = 23.73;
+            double y = 37.27;
             Console.WriteLine("Hello World, Alexander!");
+            Console.WriteLine("SumTwoNum of ", x.ToString(), "and ", y.ToString(), "= ", SumTwoNum(x,y));
         }
     }
 }
